9601b133697d70c8cc7c6d12b00a72f6202c9a7d
Akash Danao <akash@aitglobalinc.com>
2017-11-09 19:05:11 +0530

    create a user model and save it to mongodb

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../models/user');
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
 });
 
-router.get('/message/:msg', function (req, res, next) {
-    res.render('node', {message : req.params.msg});
-});
-
-router.post('/message', function(req, res, next){
-    var message = req.body.message;
-    res.redirect('/message/' + message);
+router.post('/', function(req, res, next){
+    var email = req.body.email;
+    var user = new User({
+        firstName : 'Akash',
+        lastName : 'Danao',
+        email : email,
+        password : 'secret'
+    }); 
+    user.save(function(err, res){
+        console.log(' result ' + res);
+        console.log(' err ' + err);
+    });
+    res.redirect('/'); 
 });
 
 module.exports = router;
