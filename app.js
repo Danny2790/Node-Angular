c6a6665be3801db43f0e586e2a61847a783680bf
Akash Danao <akash@aitglobalinc.com>
2017-11-09 19:04:41 +0530

    fix mongodb connection uri

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var appRoutes = require('./routes/app');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular')
+mongoose.connect('mongodb://localhost:27017/admin',{ useMongoClient: true })
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
