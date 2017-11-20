51a6e427f44aaad873eed3b95022e6bb3e2a2e5b
Akash Danao <akash@aitglobalinc.com>
2017-11-20 18:55:12 +0530

    add email validation pattern

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -15,7 +15,10 @@ export class SigninComponent implements OnInit{
     
     ngOnInit(){
         this.myForm = new FormGroup({
-            email : new FormControl(null, Validators.required),
+            email : new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)")
+            ]),
             password : new FormControl(null, Validators.required),
         });
     }
