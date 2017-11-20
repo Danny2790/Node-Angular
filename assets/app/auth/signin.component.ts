b617d829f998e598fcfb84def77757517c980c4a
Akash Danao <akash@aitglobalinc.com>
2017-11-20 18:43:56 +0530

    Sign in page add reactiveforms for email and password

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector : 'app-signin',
     templateUrl : './signin.Component.html'
 })
-export class SigninComponent {
-
+export class SigninComponent implements OnInit{
+    myForm : FormGroup;
+    
+    onSubmit(){
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+    
+    ngOnInit(){
+        this.myForm = new FormGroup({
+            email : new FormControl(null, Validators.required),
+            password : new FormControl(null, Validators.required),
+        });
+    }
 }
